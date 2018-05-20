bf8558d5a012c8a6f4e022ac4b80584bf48d5df1
Tim Pringle <tim.pringle@powershell.amsterdam>
2018-05-20 08:35:40 +0200

    Added convert package to allow multiple endpoints to use the same code

---
+++ convert/resources.go
@@ -0,0 +1,163 @@
+package convert
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/transcribeservice"
+	"github.com/tim-pringle/go-aws/transcribe"
+	"github.com/tim-pringle/go-misc/misc"
+)
+
+var (
+	ErrJobnameNotProvided = errors.New("No job number has been provided HTTP body")
+	ErrTranscribeRunning  = errors.New("The transcription job is running")
+	ErrTranscribeFailure  = errors.New("The transcription job has failed")
+	ErrJobDoesntExist     = errors.New("The transcripting job does not exist")
+)
+
+//Convert receives a Transcribe job identifier, retrieves the job data and then converts
+//the transcription content into an srt format, which is returned to the caller
+func Convert(jobname string) (string, error) {
+
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Processing conversion request")
+
+	sess, _ := session.NewSessionWithOptions(session.Options{
+		Config:  aws.Config{Region: aws.String("eu-west-1")},
+		Profile: "development",
+	})
+
+	log.Printf("Job name : %s", jobname)
+	log.Printf("Creating new session")
+	transcriber := transcribeservice.New(sess)
+
+	log.Printf("Getting transcription job")
+	transcriptionjobinput := transcribeservice.GetTranscriptionJobInput{
+		TranscriptionJobName: &jobname,
+	}
+	transcriptionjoboutput, err := transcriber.GetTranscriptionJob(&transcriptionjobinput)
+
+	if err != nil {
+		ErrMsg := errors.New(err.Error())
+		log.Printf("%s", err.Error())
+		return "", ErrMsg
+	}
+
+	strStatus := *(transcriptionjoboutput.TranscriptionJob.TranscriptionJobStatus)
+	log.Printf("Job status is %s", strStatus)
+
+	if strStatus == "FAILED" {
+		return "", ErrTranscribeFailure
+	}
+	if strStatus == "IN_PROGRESS" {
+		return "", ErrTranscribeRunning
+	}
+
+	var uri *string
+	uri = transcriptionjoboutput.TranscriptionJob.Transcript.TranscriptFileUri
+	log.Printf("URI is %s", *uri)
+
+	response, _ := http.Get(*uri)
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	//If there's an error, print the error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// initialize our variable to hold the json
+	var awstranscript transcribe.Awstranscript
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'awstranscript' which we defined above
+	json.Unmarshal(body, &awstranscript)
+
+	var transcription []transcribe.Item
+	transcription = awstranscript.Results.Items
+
+	var index, sequence int = 0, 0
+	var srtinfo, subdetail, subtitle, sttime, classification, text, entime string
+	var strlen int
+	var firstrow bool
+
+	for index = 0; index < len(transcription); {
+		//Variable initiation for length of subtitle text, sequence number if its the first line and the subtitle text
+
+		sequence++
+		firstrow = true
+		subtitle = ""
+
+		//Grab the start time, convert it to a number, then convert the number an SRT valid time string
+		sttime = transcription[index].Starttime
+		fsttime, err := strconv.ParseFloat(sttime, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		sttime = misc.Getsrttime(fsttime)
+
+		/*Repeat this until we have either reached the last item in results
+		#or the length of the lines we are reading is greater than 64 characters */
+
+		for strlen = 0; (strlen < 64) && (index < len(transcription)); {
+			text = transcription[index].Alternatives[0].Content
+			strlen += len(text)
+
+			switch classification {
+
+			case "punctuation":
+				if len(subtitle) > 0 {
+					runes := []rune(subtitle)
+					subtitle = string(runes[1 : len(subtitle)-1])
+				} else {
+					subtitle += text
+				}
+			default:
+				subtitle += (text + " ")
+			}
+
+			//If the length of the current string is greater than 32 and this
+			//is the first line of the sequence, then add a return character to it
+
+			if (strlen > 32) && (firstrow == true) {
+				subtitle += "\n"
+				firstrow = false
+			}
+
+			/*If the last character is a '.', then we need to set
+			the end time attribute to the previous indexes one
+			since punctuation characters to not have a time stamp*/
+
+			if classification == "punctuation" {
+				entime = transcription[index-1].Endtime
+			} else {
+				entime = transcription[index].Endtime
+			}
+
+			fsttime, err = strconv.ParseFloat(entime, 64)
+			entime = misc.Getsrttime(fsttime)
+
+			index++
+		}
+		//Setup the string that is refers to these two
+		//lines in SRT format
+
+		subdetail = fmt.Sprintf("\n%d\n%s --> %s\n%s\n", sequence, sttime, entime, subtitle)
+
+		//Append this to the existing string
+		srtinfo += subdetail
+
+	}
+
+	log.Printf(srtinfo)
+
+	return srtinfo, nil
+}
