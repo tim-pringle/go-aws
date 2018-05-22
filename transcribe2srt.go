0bbecaf33e881caeac37e627c6d72fdc865c62af
Tim Pringle <tim.pringle@powershell.amsterdam>
2018-05-22 15:39:12 +0200

    Removed GUID function from pacakge

--- transcribe2srt.go
+++ transcribe2srt.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -252,17 +251,4 @@ type Item struct {
 type Alternative struct {
 	Confidence string `json:"confidence"`
 	Content    string `json:"content"`
-}
-
-//GUID - generates a unique identifier
-func GUID() (guid string) {
-	ad, err := time.Parse("02-01-2006", "01-01-1970")
-
-	if err != nil {
-	}
-
-	timesince := time.Since(ad).Nanoseconds()
-	strsince := strconv.FormatInt(timesince, 10)
-	guid = fmt.Sprintf("0" + strsince[0:4] + "-" + strsince[4:9] + "-" + strsince[9:14] + "-" + strsince[14:19])
-	return
 }
